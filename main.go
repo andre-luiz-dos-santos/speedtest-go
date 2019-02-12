2f31dc337d179435d69cee58d1da1d38e77dac3d
Andre Luiz dos Santos <andre@netvision.com.br>
2019-02-12 19:02:59 -0200

    A web server that redirects every request

--- main.go
+++ main.go
@@ -7,14 +7,30 @@ import (
 )
 
 var (
-	webDir      string
-	bindAddress string
+	webBindAddr   string
+	webDir        string
+	redirBindAddr string
+	redirURL      string
 )
 
+func startRedirector() {
+	s := &http.Server{
+		Addr:    redirBindAddr,
+		Handler: http.RedirectHandler(redirURL, http.StatusPermanentRedirect),
+	}
+	log.Fatal(s.ListenAndServe())
+}
+
 func main() {
-	flag.StringVar(&bindAddress, "bind", ":8080", "bind address")
-	flag.StringVar(&webDir, "root", "static", "root web directory")
+	flag.StringVar(&webBindAddr, "web-bind", ":8080", "web bind address")
+	flag.StringVar(&webDir, "web-root", "static", "web root directory")
+	flag.StringVar(&redirBindAddr, "redir-bind", "", "redirector bind address")
+	flag.StringVar(&redirURL, "redir-url", "", "redirector target URL")
 	flag.Parse()
+
+	if redirBindAddr != "" && redirURL != "" {
+		go startRedirector()
+	}
 
 	http.HandleFunc("/empty.php", emptyHandler)
 	http.HandleFunc("/garbage.php", garbageHandler)
@@ -22,5 +38,5 @@ func main() {
 	fs := http.FileServer(http.Dir(webDir))
 	http.Handle("/", fs)
 
-	log.Fatal(http.ListenAndServe(bindAddress, nil))
+	log.Fatal(http.ListenAndServe(webBindAddr, nil))
 }
