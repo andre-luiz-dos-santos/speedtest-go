769a571aee4e2e38129186330c17573c64b41fe3
André <andre@fastville.com.br>
2020-04-30 17:32:43 -0300

    Also send access denied errors to stderr

--- wrapper_allow_network.go
+++ wrapper_allow_network.go
@@ -1,11 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func allowNetworkWrapper(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if allowFrom.IsSet() && !allowFrom.ContainsTCPAddr(r.RemoteAddr) {
-			http.Error(w, "Access denied from "+r.RemoteAddr, http.StatusForbidden)
+			message := "Access denied from " + r.RemoteAddr
+			log.Print(message)
+			http.Error(w, message, http.StatusForbidden)
 			return
 		}
 		h.ServeHTTP(w, r)
